6d48542bbc534df43be401909713b0057a03f7e4
Rohan Garg <rohan.garg@intel.com>
2023-06-06 23:11:14 +0200

    anv: split ANV_PIPE_RENDER_TARGET_BUFFER_WRITES for finer grained flushing
    split ANV_PIPE_RENDER_TARGET_BUFFER_WRITES into separate CS_STALL,
    RT_FLUSH & TILE_FLUSH flags in order to have finer control over cache
    coherency.
    
    Tigerlake CS has it's own cache fetching directly from the memory controller,
    so we need to do a tile flush to ensure the query data is visible.
    
    This fixes test_resolve_non_issued_query_data in vkd3d on TGL.
    
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>
    Fixes: 3c4c18341aaf ("anv: narrow flushing of the render target to buffer writes")
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23500>
    (cherry picked from commit d0e0ba897fd5a15bc5283b150a122dee078006c2)

--- src/intel/vulkan/anv_blorp.c
+++ src/intel/vulkan/anv_blorp.c
@@ -534,6 +534,16 @@ void anv_CmdCopyBufferToImage2(
    anv_blorp_batch_finish(&batch);
 }
 
+static void
+anv_add_buffer_write_pending_bits(struct anv_cmd_buffer *cmd_buffer,
+                                  const char *reason)
+{
+   const struct intel_device_info *devinfo = cmd_buffer->device->info;
+
+   cmd_buffer->state.pending_query_bits |=
+      ANV_QUERY_RENDER_TARGET_WRITES_PENDING_BITS(devinfo);
+}
+
 void anv_CmdCopyImageToBuffer2(
     VkCommandBuffer                             commandBuffer,
     const VkCopyImageToBufferInfo2*             pCopyImageToBufferInfo)
@@ -553,9 +563,7 @@ void anv_CmdCopyImageToBuffer2(
 
    anv_blorp_batch_finish(&batch);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "after copy image to buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "after copy image to buffer");
 }
 
 static bool
@@ -780,9 +788,7 @@ void anv_CmdCopyBuffer2(
 
    anv_blorp_batch_finish(&batch);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "after copy buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "after copy buffer");
 }
 
 
@@ -844,9 +850,7 @@ void anv_CmdUpdateBuffer(
 
    anv_blorp_batch_finish(&batch);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "update buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "update buffer");
 }
 
 void
@@ -954,9 +958,8 @@ void anv_CmdFillBuffer(
                             anv_address_add(dst_buffer->address, dstOffset),
                             fillSize, data);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "after fill buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "after fill buffer");
+
 }
 
 void anv_CmdClearColorImage(
