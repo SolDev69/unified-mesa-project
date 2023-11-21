92f284c04d1a2cc252ff2c2d5b334e4c43bb7ca4
Boris Brezillon <boris.brezillon@collabora.com>
2023-11-21 10:06:04 +0100

    pan/kmod: Add a backend for panthor
    Panthor is a new kernel driver handling CSF-based GPUs. It's designed
    around the new VM model where:
    
    - VM management is all explicit (you get to choose where objects are
      mapped in the GPU VA space)
    - synchronization is explicit too (there's not BO_WAIT, and we don't
      pass BOs around to serve as implicit deps)
    
    We add a few panthor specific helpers (those exposed in panthor_kmod.h)
    too:
    
    - panthor_kmod_xxx_sync_point() are needed to make pan_kmod_bo_wait()
      work with the new synchronization/VM model
    - panthor_kmod_get_flush_id() is exposing the LATEST_FLUSH_ID register
    - panthor_kmod_vm_handle() is providing a way to query the VM handle
      attached to the pan_kmod_vm object (needed for a few panthor ioctls)
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

---
+++ src/panfrost/lib/kmod/panthor_kmod.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2023 Collabora, Ltd.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+struct drm_panthor_csif_info;
+
+struct pan_kmod_bo;
+struct pan_kmod_dev;
+struct pan_kmod_vm;
+
+int panthor_kmod_bo_attach_sync_point(struct pan_kmod_bo *bo,
+                                      uint32_t sync_handle,
+                                      uint64_t sync_point, bool read_only);
+int panthor_kmod_bo_get_sync_point(struct pan_kmod_bo *bo,
+                                   uint32_t *sync_handle, uint64_t *sync_point,
+                                   bool read_only);
+void panthor_kmod_vm_new_sync_point(struct pan_kmod_vm *vm,
+                                    uint32_t *sync_handle,
+                                    uint64_t *sync_point);
+uint32_t panthor_kmod_get_flush_id(const struct pan_kmod_dev *dev);
+
+const struct drm_panthor_csif_info *
+panthor_kmod_get_csif_props(const struct pan_kmod_dev *dev);
+
+#if defined(__cplusplus)
+} // extern "C"
+#endif
