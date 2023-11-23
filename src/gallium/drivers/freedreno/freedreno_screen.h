565ea025b084722a0c42e35da95b1acc48bb20fe
SolDev69 <michaelcraft1104@gmail.com>
2023-11-23 09:48:44 -0500

    test 4

--- src/gallium/drivers/freedreno/freedreno_screen.h
+++ src/gallium/drivers/freedreno/freedreno_screen.h
@@ -27,9 +27,9 @@
 #ifndef FREEDRENO_SCREEN_H_
 #define FREEDRENO_SCREEN_H_
 
-// #include "common/freedreno_dev_info.h"
-// #include "drm/freedreno_drmif.h"
-// #include "drm/freedreno_ringbuffer.h"
+#include "common/freedreno_dev_info.h"
+#include "drm/freedreno_drmif.h"
+#include "drm/freedreno_ringbuffer.h"
 #include "perfcntrs/freedreno_perfcntr.h"
 
 #include "pipe/p_screen.h"
