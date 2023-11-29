cdfec077c96206cde501b2e84597d9f31b00834c
SolDev69 <michaelcraft1104@gmail.com>
2023-11-29 17:52:45 -0500

    cleanup 12

--- src/util/u_debug_stack_android.cpp
+++ src/util/u_debug_stack_android.cpp
@@ -24,7 +24,6 @@
 #include "u_debug_stack.h"
 
 #if WITH_LIBBACKTRACE
-
 #include "util/simple_mtx.h"
 #include "util/u_debug.h"
 #include "util/hash_table.h"
@@ -53,14 +52,12 @@ debug_backtrace_capture(debug_stack_frame *backtrace,
                         unsigned start_frame,
                         unsigned nr_frames)
 {
-
 }
 
 void
 debug_backtrace_dump(const debug_stack_frame *backtrace,
                      unsigned nr_frames)
 {
-
 }
 
 void
@@ -68,7 +65,6 @@ debug_backtrace_print(FILE *f,
                       const debug_stack_frame *backtrace,
                       unsigned nr_frames)
 {
-
 }
 
 #else
