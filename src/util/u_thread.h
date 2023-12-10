bcd896f3343c24193ac4a7bde27abd205ca2ae44
SolDev69 <michaelcraft1104@gmail.com>
2023-12-10 12:52:02 -0500

    fix pthread

--- src/util/u_thread.h
+++ src/util/u_thread.h
@@ -116,7 +116,7 @@ static inline bool u_thread_is_self(thrd_t thread)
  * util_barrier
  */
 
-#if defined(HAVE_PTHREAD) && !defined(__APPLE__) && !defined(__HAIKU__)
+#if defined(HAVE_PTHREAD) && !defined(__APPLE__) && !defined(__HAIKU__) && !defined(__ANDROID__)
 
 typedef pthread_barrier_t util_barrier;
 
