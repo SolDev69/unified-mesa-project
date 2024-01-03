6b59ea87bd7d8cb08b0d71398c44477626940592
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2024-01-03 20:33:53 +0000

    add panfrost unknown model support

--- src/panfrost/lib/pan_props.c
+++ src/panfrost/lib/pan_props.c
@@ -77,6 +77,15 @@ const struct panfrost_model panfrost_model_list[] = {
 };
 /* clang-format on */
 
+const struct panfrost_model panfrost_unknown_model = {
+   .gpu_id = 0,
+   .name = "Unknowm Mali device (Panfrost)",
+   .performance_counters = "AAAA",
+   .min_rev_anisotropic = NO_ANISO, 
+   .tilebuffer_size = 8192, 
+   .quirks = {}, 
+};
+
 #undef NO_ANISO
 #undef HAS_ANISO
 #undef MODEL
@@ -93,7 +102,7 @@ panfrost_get_model(uint32_t gpu_id)
          return &panfrost_model_list[i];
    }
 
-   return NULL;
+   return &panfrost_unknown_model;
 }
 
 unsigned
