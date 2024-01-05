0bc813906c96313359c6a505137320347b270657
Kenneth Graunke <kenneth@whitecape.org>
2024-01-05 14:08:47 -0800

    intel/brw: Fix opt_split_sends() to allow for FIXED_GRF send sources
    opt_copy_propagation() can sometimes propagate FIXED_GRF sources into
    SHADER_OPCODE_SENDs as the message payload.  For example, GS input
    reads, which simply take a URB handle and have the offset in the
    descriptor.  For non-VGRFs, there isn't a payload to split, so just
    skip past such send messages.
    
    Fixes: 589b03d02f0 ("intel/fs: Opportunistically split SEND message payloads")
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28067>
    (cherry picked from commit ba11127944d898581eab17dc76ea56372b97d0bd)

--- src/intel/compiler/brw_fs.cpp
+++ src/intel/compiler/brw_fs.cpp
@@ -3049,10 +3049,9 @@ fs_visitor::opt_split_sends()
 
    foreach_block_and_inst(block, fs_inst, send, cfg) {
       if (send->opcode != SHADER_OPCODE_SEND ||
-          send->mlen <= reg_unit(devinfo) || send->ex_mlen > 0)
+          send->mlen <= reg_unit(devinfo) || send->ex_mlen > 0 ||
+          send->src[2].file != VGRF)
          continue;
-
-      assert(send->src[2].file == VGRF);
 
       /* Currently don't split sends that reuse a previously used payload. */
       fs_inst *lp = (fs_inst *) send->prev;
