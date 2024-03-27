alexvorxx/zink-xlib-termux

nvk: fix valve segfault from setting a descriptor set from NULL
Reported by Nikita Vilunov and fix found by him when analyzing his
CS2 dump.

cc: mesa-stable

v2: these two need to be zero when set == NULL

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10719
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28353>

src/nouveau/vulkan/nvk_cmd_buffer.c
@@ -636,8 +636,13 @@ nvk_CmdBindDescriptorSets(VkCommandBuffer commandBuffer,
       VK_FROM_HANDLE(nvk_descriptor_set, set, pDescriptorSets[i]);
 
       if (desc->sets[s] != set) {
-         desc->root.sets[s] = nvk_descriptor_set_addr(set);
-         desc->set_sizes[s] = set->size;
+         if (set != NULL) {
+            desc->root.sets[s] = nvk_descriptor_set_addr(set);
+            desc->set_sizes[s] = set->size;
+         } else {
+            desc->root.sets[s] = 0;
+            desc->set_sizes[s] = 0;
+         }
          desc->sets[s] = set;
          desc->sets_dirty |= BITFIELD_BIT(s);
 

SolDev69/unified-mesa-project

(cherry picked from commit 561fae6845479b81d8f41f23376c469524004166)

src/nouveau/vulkan/nvk_cmd_buffer.c
@@ -580,8 +580,13 @@ nvk_CmdBindDescriptorSets(VkCommandBuffer commandBuffer,
          vk_to_nvk_descriptor_set_layout(pipeline_layout->set_layouts[set_idx]);
 
       if (desc->sets[set_idx] != set) {
-         desc->root.sets[set_idx] = nvk_descriptor_set_addr(set);
-         desc->set_sizes[set_idx] = set->size;
+         if (set != NULL) {
+            desc->root.sets[set_idx] = nvk_descriptor_set_addr(set);
+            desc->set_sizes[set_idx] = set->size;
+         } else {
+            desc->root.sets[set_idx] = 0;
+            desc->set_sizes[set_idx] = 0;
+         }
          desc->sets[set_idx] = set;
          desc->sets_dirty |= BITFIELD_BIT(set_idx);
 