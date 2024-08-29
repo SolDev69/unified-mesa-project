adc463991a41a9e19db2f5780541cbee01271f4e
SolDev69 <michaelcraft1104@gmail.com>
2024-08-29 00:26:26 -0400

    ignore hash many neon????

--- src/util/blake3/blake3_dispatch.c
+++ src/util/blake3/blake3_dispatch.c
@@ -264,12 +264,6 @@ void blake3_hash_many(const uint8_t *const *inputs, size_t num_inputs,
 #endif
 #endif
 
-#if BLAKE3_USE_NEON == 1
-  blake3_hash_many_neon(inputs, num_inputs, blocks, key, counter,
-                        increment_counter, flags, flags_start, flags_end, out);
-  return;
-#endif
-
   blake3_hash_many_portable(inputs, num_inputs, blocks, key, counter,
                             increment_counter, flags, flags_start, flags_end,
                             out);
